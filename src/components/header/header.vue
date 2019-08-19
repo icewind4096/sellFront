05fa1e6b5394f4ad706edee0abd5610e8f7c27a9
wangj <icewind4096@gmail.com>
2019-08-19 22:10:22 +0800

    制作第一个页面

---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    我是Header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'header'
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
