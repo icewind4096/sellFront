fb81831c8da1d588a16e2c33d4a4bbd3d743ade4
iceind4096 <icewind4096@gmail.com>
2019-08-24 17:59:01 +0800

    Header组件全部完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -35,16 +35,43 @@
       <img width="100%" height="100%" :src="seller.avatar">
     </div>
 
-    <div v-show="detailShow" class="detail">
+    <div v-show="detailShow" class="detail" transition ="fade">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
           <div class="star-wrapper">
             <star :size="48" :score="seller.score"></star>
           </div>
+
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+
+          <ul v-if="seller.supports" class="supports">
+            <li v-for="item in seller.supports" class="support-item">
+              <div>
+                <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+                <span class="text">{{seller.supports[$index].description}}</span>
+              </div>
+            </li>
+          </ul>
+
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+
+          <div class="bulletin">
+            <p class="content">
+              {{seller.bulletin}}
+            </p>
+          </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" @click="hideDetail">
         <i class="icon-close"></i>
       </div>
     </div>
@@ -72,10 +99,13 @@
     methods: {
       showDetail() {
         this.detailShow = true;
+      },
+      hideDetail() {
+        this.detailShow = false;
       }
     },
     components: {
-      star
+      'star': star
     }
   };
 </script>
@@ -203,7 +233,13 @@
       width: 100%
       height: 100%
       overflow: auto
-      background: rgba(7, 17, 27, 0.8)
+      transition: all 1s
+      &.fade-transition
+        opacity: 1;
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter,&.fade-leave
+        opacity: 0;
+        background: rgba(7, 17, 27, 0)
       .detail-wrapper
         width: 100%
         min-height: 100%
@@ -212,14 +248,68 @@
           padding-bottom: 64px
           .name
             line-height: 16px
+            text-align: center
             font-size: 16px
             font-weight: 700
-            text-align: center
+            padding-bottom: 10px
           .star-wrapper
             padding-bottom: 18px
             padding: 2px 0
             text-align: center
-      .detail-close
+          .title
+            display: flex
+            width: 80%
+            margin: 28px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding: 0 12px
+              font-weight: 700
+              font-size: 14px
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+            .icon
+              display: inline-block
+              vertical-align: top
+              width: 16px
+              height: 16px
+              margin-right: 6px
+              background-size: 16px 16px
+              background-repeat: no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              display: inline-block
+              line-height: 15px
+              font-size: 15px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px 0 12px
+              line-height: 24px
+              font-size: 12px
+              font-weight: 200
+              color: rgb(255, 255, 255)
+  .detail-close
         position: relative
         width: 32px
         height: 32px
