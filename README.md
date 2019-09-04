16720d1d281c9dc7464a21134b48c532b14582b9
wangj <icewind4096@gmail.com>
2019-09-04 22:27:12 +0800

    动画小球，再次修改

--- README.md
+++ README.md
@@ -27,4 +27,23 @@ npm test
 #坑点
 1.注意要是保持驼峰命名，驼峰字母前面要加-,HTML不区分大小写这个是原因
 
+2.为了解决在PC端点击时触发两次, 使用event._constructed,如果_constructed说明是原生的事件，不处理，直接返回
+
+#使用组件的顺序
+1.import组件
+  import 组件变量名 from 组件文件路径
+  Ps. import cartControl from '../../components/cartControl/cartcontrol.vue';
+
+2.注册组件
+  components: {
+        '组件变量名': 组件名称(相当于类名)
+  }
+   
+3.通过Vue.set(变量, '属性', 值)，设置变量不存在的属性值是，可以被Vue观察到，从而导致界面刷新
+
+#CSS
+1. font-size: 0px 为了去掉元素之间的间隙
+2. padding: 有时候可以在元素不增大的基础上，增大操作区域
+   
+
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/cartControl/cartcontrol.vue
+++ src/components/cartControl/cartcontrol.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease" v-show="food.count > 0" @click="decCart" transition="move">
-      <span class="inner icon-remove_circle_outline"></span>
+    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count > 0" @click="decCart" transition="move">
     </div>
 
     <div class="cart-count" v-show="food.count > 0">
@@ -26,7 +25,7 @@
     },
     methods: {
       incCart(event) {
-        if (!event._constructed) { return; } // 为了解决在PC端点击时触发两次 如果_constructed说明是原生的事件，不处理，直接返回
+        if (!event._constructed) { return; }
         if (!this.food.count) {
           Vue.set(this.food, 'count', 1);
           this.food.count = 1;
@@ -51,22 +50,8 @@
     .cart-decrease
       display: inline-block
       padding: 6px
-      transition: all 0.4s linear
-      &.move-transition
-        opacity: 1
-        transform: translate3d(0, 0, 0)
-        .inner
-          display: inline-block
-          line-height: 24px
-          font-size: 24px
-          color: rgb(0, 160, 220)
-          transition: all 0.4s linear
-          transform: rotate(0)
-      &.move-enter, &.move-leave
-        opacity: 0
-        transform: translate3d(24px, 0, 0)
-        .inner
-          transform: rotate(180deg)
+      line-height: 24px
+      font-size: 24px
     .cart-count
       display: inline-block
       vertical-align: top
@@ -83,3 +68,23 @@
       font-size: 24px
       color: rgb(0, 160, 220)
 </style>
+
+<!--
+    .cart-decrease
+      display: inline-block
+      transition: all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform: translate3d(0, 0, 0)
+        .inner
+          display: inline-block
+          line-height: 24px
+          font-size: 24px
+          transition: all 0.4s linear
+          transform: rotate(0)
+      &.move-enter, &.move-leave
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
+-->
