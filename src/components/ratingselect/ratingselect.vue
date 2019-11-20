97e466859c7893970ef7bf0b9942b732420756ea
wangj <icewind4096@gmail.com>
2019-11-20 22:03:47 +0800

    完成onlyContent样式

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -5,8 +5,8 @@
         <span class="block positive" :class="{'active':selectType === 0}">{{desc.positive}}<span class="count">57</span></span>
         <span class="block negative" :class="{'active':selectType === 1}">{{desc.negative}}<span class="count">67</span></span>
       </div>
-      <div class="switch">
-        <span class=".icon-check_circle"></span>
+      <div class="switch" :class="{'on':onlyContent === true}">
+        <span class="icon-check_circle"></span>
         <span class="text">只看有内容的评价</span>
       </div>
     </div>
@@ -52,7 +52,7 @@
   @import "../../common/stylus/mixin.styl"
   .ratingselect
     .rating-type
-      padding: 18px, 0
+      padding: 18px 0
       margin: 0 18px
       border-1px(rgba(7, 17, 27, 0.1))
       font-size: 0
@@ -77,4 +77,22 @@
           background: rgba(77, 85, 93, 0.2)
           &.active
             background: rgb(77, 85, 93)
+    .switch
+      padding: 12px 18px
+      line-height: 24px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      &.on
+        .icon-check_circle
+          color: #00c850
+      .icon-check_circle
+        display: inline-block
+        vertical-align: top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
 </style>
